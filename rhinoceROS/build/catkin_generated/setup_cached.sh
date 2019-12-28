0cecef5dd6d9da59d27b249e87f83aee8654ea9b
Mars-Rover-Manipal <electronics.marsrovermanipal@gmail.com>
2019-12-28 23:05:19 +0530

    .

---
+++ rhinoceROS/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/leander/rhinoceROS/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/leander/rhinoceROS/build"
+export PYTHONPATH="/home/leander/rhinoceROS/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages:/home/leander/catkin_ws/devel/lib/python2.7/dist-packages"
---
+++ rhinoceROS/build/ros_service/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leander/rhinoceROS/src/ros_service
+# Build directory: /home/leander/rhinoceROS/build/ros_service
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
