eb3c87eea014dcc0e1d2674fee73773442e0b8aa
Mars-Rover-Manipal <electronics.marsrovermanipal@gmail.com>
2019-12-29 09:09:50 +0530

    .

--- rhinoceROS/build/ros_service/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/leander/rhinoceROS/install/include".split(';') if "/home/leander/rhinoceROS/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "ros_service"
-PROJECT_SPACE_DIR = "/home/leander/rhinoceROS/install"
-PROJECT_VERSION = "0.0.0"
