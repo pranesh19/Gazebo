9b482d5b6cc87bce9a9da8dfb533ddbc1365e9f8
Mars-Rover-Manipal <electronics.marsrovermanipal@gmail.com>
2020-01-05 18:35:15 +0530

    .

---
+++ rhinoceROS/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/leander/rhinoceROS/src
+# Build directory: /home/leander/rhinoceROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_service")
+subdirs("realsense_gazebo_plugin")
